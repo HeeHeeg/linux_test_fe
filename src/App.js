30a4a27f50fa333d3af84ca4f91849f6239a1826
HeeHeeg <qmffndoden@naver.com>
2023-10-05 12:13:29 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <div>
+    <input type="text" placeholder="name"/>
+    <input type="text" placeholder="desc"/>
+    <input type="submit"/>
+  </div>
+
+}
+
+export default App;
